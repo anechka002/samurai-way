de7b7cfda78c0dccfdd126a9448de137b0389e9e
Anna <blinova.anneta@gmail.com>
2025-08-14 15:20:36 +0200

    `feat: Update Routing component with lazy loading and suspense`

--- src/routing/Routing.tsx
+++ src/routing/Routing.tsx
@@ -1,26 +1,38 @@
 import { Routes, Route } from "react-router"
-import { Dialogs, Login, News, Profile, ProtectedRoute, Users } from "@/components"
+import { Login, News, ProtectedRoute } from "@/components"
 import { PATH } from "@/constants"
 import { useAppSelector } from "@/hooks"
+import { Preloader } from "@/components/common"
+import { lazy, Suspense } from "react"
+
+const Users = lazy(() => import("../components/users/Users")
+    .then(module => ({ default: module.Users })));
+const Dialogs = lazy(() => import("../components/dialogs/Dialogs")
+    .then(module => ({ default: module.Dialogs })));
+const Profile = lazy(() => import("../components/profile/Profile")
+    .then(module => ({ default: module.Profile })));
+
 
 export const Routing = () => {
   const isAuth = useAppSelector((state) => state.auth.isAuth)
   return (
-    <Routes>
-      {/* Private routes. Если пользователь не залогинен, то он не попадает в этот роуты  */}
-      <Route element={<ProtectedRoute isAllowed={isAuth} redirectPath={PATH.LOGIN}/>}>
-        <Route path={PATH.DIALOGS} element={<Dialogs />} />
-        <Route path={PATH.PROFILE} element={<Profile />} />
-        <Route path={'/profile/:userId'} element={<Profile />} />
-      </Route>
-      {/* Private routes. Если пользователь залогинен, то он не попадает в этот роут */}
-      <Route element={<ProtectedRoute isAllowed={!isAuth} redirectPath={PATH.PROFILE}/>}>
-        <Route path={PATH.LOGIN} element={<Login />} />
-      </Route>
+    <Suspense fallback={ <div><Preloader /></div>}>
 
-      <Route path={PATH.USERS} element={<Users />} />
-      <Route path={PATH.NEWS} element={<News />} />
+      <Routes>
+        {/* Private routes. Если пользователь не залогинен, то он не попадает в этот роуты  */}
+        <Route element={<ProtectedRoute isAllowed={isAuth} redirectPath={PATH.LOGIN} />}>
+          <Route path={PATH.DIALOGS} element={<Dialogs />} />
+          <Route path={PATH.PROFILE} element={<Profile />} />
+          <Route path={"/profile/:userId"} element={<Profile />} />
+        </Route>
+        {/* Private routes. Если пользователь залогинен, то он не попадает в этот роут */}
+        <Route element={<ProtectedRoute isAllowed={!isAuth} redirectPath={PATH.PROFILE} />}>
+          <Route path={PATH.LOGIN} element={<Login />} />
+        </Route>
 
-    </Routes>
+        <Route path={PATH.USERS} element={<Users />} />
+        <Route path={PATH.NEWS} element={<News />} />
+      </Routes>
+    </Suspense>
   )
 }
